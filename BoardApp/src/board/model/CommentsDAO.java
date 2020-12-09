8779d50269748d9dd2fa3182e87d634ed58cecdb
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-12-09 14:21:20 +0900

    c

--- BoardApp/src/board/model/CommentsDAO.java
+++ BoardApp/src/board/model/CommentsDAO.java
@@ -2,7 +2,10 @@ package board.model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import db.DBManager;
 
@@ -30,7 +33,48 @@ public class CommentsDAO {
 		}
 		return result;
 	}
+	
+	//뉴스한 건에 소속된 하위 코멘트 목록 가져오기
+	public List selectAll(int news_id) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs = null;
+		ArrayList list = new ArrayList();
+		String sql="select * from comments where news_id=?";
+		
+		con=manager.getConnection();
+		try {
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, news_id);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				Comments comments = new Comments();
+				comments.setComments_id(rs.getInt("comments_id"));
+				comments.setNews_id(rs.getInt("news_id")); //fk
+				comments.setAuthor(rs.getString("author"));
+				comments.setMsg(rs.getString("msg"));
+				comments.setCdate(rs.getString("cdate"));
+				list.add(comments);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			manager.release(con, pstmt, rs);
+		}
+		return list;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
